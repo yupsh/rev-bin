506b1703657718f3b8947c8d71a846dfa63c3c3c
nicerobot <nicerobot@users.noreply.github.com>
2025-11-07 15:47:10 -0800

    update

---
+++ README.md
@@ -0,0 +1,20 @@
+# yup-rev
+
+```
+NAME:
+   rev - reverse lines characterwise
+
+USAGE:
+   rev [OPTIONS] [FILE...]
+
+      The rev utility copies the specified files to standard output, reversing
+      the order of characters in every line. If no files are specified, standard
+      input is read.
+
+COMMANDS:
+   help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --separate, -s  reverse characters separately for each line (default: false)
+   --help, -h      show help
+```
---
+++ yup-rev/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+
+	yup "github.com/gloo-foo/framework"
+	. "github.com/yupsh/rev"
+)
+
+const (
+	flagSeparate = "separate"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "rev",
+		Usage: "reverse lines characterwise",
+		UsageText: `rev [OPTIONS] [FILE...]
+
+   The rev utility copies the specified files to standard output, reversing
+   the order of characters in every line. If no files are specified, standard
+   input is read.`,
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    flagSeparate,
+				Aliases: []string{"s"},
+				Usage:   "reverse characters separately for each line",
+			},
+		},
+		Action: action,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "rev: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func action(c *cli.Context) error {
+	var params []any
+
+	// Add file arguments (or none for stdin)
+	for i := 0; i < c.NArg(); i++ {
+		params = append(params, yup.File(c.Args().Get(i)))
+	}
+
+	// Add flags based on CLI options
+	if c.Bool(flagSeparate) {
+		params = append(params, Separate)
+	}
+
+	// Create and execute the rev command
+	cmd := Rev(params...)
+	return yup.Run(cmd)
+}
